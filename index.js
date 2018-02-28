5417c769e2932ed07c83cd97a4663a38c0b3dfe4
RoyaltyYT <agarbot9@gmail.com>
2018-02-28 20:37:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 var fs = require("fs");
-var express = require('express');
-
-var app = express();
-var server = app.listen(3000);
-app.use(express.static('public'));
 
 var latestkey;
 var numbera = 0;
